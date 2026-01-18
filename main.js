b382288771ab09f180e8ffbf6d1ee577d7774886
Desi <deshy_123@hotmail.com>
2026-01-18 14:52:14 +0100

    avance formulario con nombres columnas y nTareas

--- main.js
+++ main.js
@@ -10,10 +10,50 @@ osea si no hay info, que salga el form y si ya esta guardada la configuracion dl
 */
 const formulario=document.getElementById("formula");
 const numCol=document.getElementById("numCol");
+const contenedor=document.getElementById("contenedorcito");
 
 formulario.addEventListener("submit",function(e){
  e.preventDefault();
-    const valor=numCol.value;
- //numCol no puede ser ni negativo ni vacio
- console.log("num de col: "+valor);
+    const valor=parseInt(numCol.value);
+ 
+   if(isNaN(valor)){
+     alert("El campo no puede estar vacio. Por favor introduce un numero de columnas");
+     return;
+   }
+   if(valor < 1){
+      alert("La cantidad introducida de columnas no puede ser negativa. introduce un número positivo de columnas");
+      return;
+   }
+  //console.log("num de col: "+valor);
+  for(let i=1; i<=valor;i++){
+    const titulo=document.createElement("h2");
+    titulo.textContent="Columna "+i;
+    contenedor.appendChild(titulo);
+    //Creacion de inputs
+    const inputNombre=document.createElement("input");
+    inputNombre.type="text";
+    inputNombre.placeholder=`Nombre columna ${i}`;
+    inputNombre.id="idCol"+i;
+    contenedor.appendChild(inputNombre);
+     //Separacion para que se vea aesthetic 
+    const separacion=document.createElement("br");
+    contenedor.appendChild(separacion);
+
+    const inputTareas=document.createElement("input");
+    inputTareas.type="number";
+    inputTareas.placeholder="Nº Tareas de la columna";
+    inputTareas.id="colTarea"+i;
+    inputTareas.min = "1";
+    inputTareas.required = true;
+    contenedor.appendChild(inputTareas);
+  }
+    const separacion=document.createElement("br");
+    contenedor.appendChild(separacion);
+
+    const botonFinal=document.createElement("input");
+    botonFinal.type="submit";
+    botonFinal.value="Confirmar";
+    botonFinal.classList.add("botoncito");
+    contenedor.appendChild(botonFinal);
+    //Faltan las validaciones y guardar formulario
 });
